2d379ad835f976f5ad9214844cbfb99762d7c150
animore <48924959+LILCHUSHKA@users.noreply.github.com>
2021-11-21 22:54:48 +0300

    Delete Scripts directory

--- Scripts/DeathPanel.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-//Класс нужен для поиска и всё
-public class DeathPanel : MonoBehaviour
-{
-    private void OnEnable() => Time.timeScale = 0;
-
-    private void OnDisable() => Time.timeScale = 1;
-}
